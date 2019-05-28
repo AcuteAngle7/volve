8f0c072b45281c52894756783cd8e044b714a929
AcuteAngle7 <angelico.razon@gmail.com>
2019-05-27 20:58:50 -0400

    started node structure
    beginning of a node structure. no connection from node to parts yet

--- sketch.js
+++ sketch.js
@@ -1,12 +1,54 @@
 function setup() {
-  angleMode (DEGREES)
-  colorMode(RGB)
+	angleMode (DEGREES)
+	colorMode(RGB)
+
+	createCanvas(1200, 1000)
+	_green = color(154,205,50)
+	_blue = color (106,90,205)
+	_gray = color (220,220,220)
+	noFill();
+
+	creatures = []
+	var creature = {}
+	creatures.push(creature) 
+	creature.nodes = []
+	var node1 = {
+		connections: [],  
+		size: 30,
+		type: "rigid", 
+		jointRange: 30,
+		recursiveLimit: 3
+	}  
+	var connection = {
+		target: 1, 
+		positionR: 100, 
+		positionTh: 90,		
+		control1R: 50, 
+		control1Th: 45,
+		control2R: 50, 
+		control2Th: -45,
+		reflection: false,
+		terminalOnly: false
+	}
+	node1.connections.push(connection)
+	var node2 = {
+		connections: [],  
+		size: 30,
+		type: "rigid", 
+		jointRange: 30,
+		recursiveLimit: 3
+	}  
+	creature.nodes.push(node1, node2) 
+	
+	creatures[1] = JSON.parse(JSON.stringify(creature))
+	creatures[1].nodes[0].size = 40
+	
+	console.log (creatures) 
+	
+	
   
-  createCanvas(1200, 1000)
-  _green = color(154,205,50)
-  _blue = color (106,90,205)
-  _gray = color (220,220,220)
-  noFill();
+  
+  
   var a,b,c,d,e,f,g,h
   a=random(500)
   b=random(500)
@@ -33,7 +75,7 @@ function setup() {
     let ty = bezierTangent(b, d, f, h, t);
     let th = atan2(ty, tx);
     th -= 90;  
-    var r = lerp(10,30,t)
+    var r = lerp(10,100,t)
     dorsal.push([cos(th)*r+x,sin(th)*r+ y])
   
     th += 180;
@@ -43,7 +85,7 @@ function setup() {
   noStroke()
   for (var i=0; i<central.length-1; i++){
 	var t = i / steps;
-    fill(lerpColor(_green,_blue, t))
+    fill(lerpColor(_blue,_green, t))
     beginShape()
 	vertex(central[i][0],central[i][1])
 	beginShape()
@@ -59,3 +101,10 @@ function setup() {
 function draw() {
   
 } //end draw 
+
+function vector (r, th) {
+	this.r = r 
+	this.th = th
+	this.x = r * cos(th) 
+	this.y = r * sin(th)
+} 
